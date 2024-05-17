0883336da915cab2a74f658e5dce4de80a61cdc7
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-05-17 09:04:16 +0545

    new profile

--- src/components/ProfileCom.jsx
+++ src/components/ProfileCom.jsx
@@ -11,18 +11,21 @@ const ProfileCom = () => {
       </div>
       <div className="flex justify-between">
            {/* left content channel name */}
-        <div className="flex">
+        <div className="flex gap-2">
         <div className="w-[5em] h-[5em] ml-2 bg-white border-black border-2 rounded-[100%] mt-[-25px] md:w-[8em] md:h-[8em] md:ml-0">
           <img className="w-full h-full rounded-[100%]" src={img} alt="profile"/>
       </div>
-      <div className="div">
-        <h1 className="text-white font-bold text-2xl">Techno Nb</h1>
+      <div className="mt-2 md:mt-3">
+        <h1 className="text-white font-bold text-[12px] md:text-2xl">Techno Nb</h1>
+        <h6 className="text-gray-300 mt-[-3px] text-[12px] md:text-[16px]">@naresh</h6>
+        <p className="text-gray-300 mt-[-0px] text-[8px] md:text-[16px]">600k Subscribers · 220 Subscribed</p>
       </div>
       </div>
 
       {/* right content subscribed */}
-      <div className="subscriber-btn mr-2 mt-4 md:mr-40">
-        <button className="bg-purple-600 w-26 text-[20px] font-bold px-2 py-1 rounded-md">Subscribed</button>
+      <div className="w-30 h-10 text-center gap-1 px-2 rounded-md bg-purple-600 mr-2 mt-3 md:mt-7 md:mr-[40em] flex items-center justify-center hover:bg-red-200">
+        <FaUserPlus/>
+        <button className="text-[15px] font-bold py-1 rounded-md md:text-[20px]">Subscribed</button>
       </div>
       </div>
      </div>
