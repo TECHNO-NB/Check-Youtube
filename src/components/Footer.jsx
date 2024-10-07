3d3938eb2cc47144418912ee46c7eedb0e569092
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-10-07 14:50:09 +0545

    add icons on bottom in mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,66 @@
 import React from "react";
 import { AiOutlineHome, AiOutlineLike } from "react-icons/ai";
 import { IoIosAddCircleOutline } from "react-icons/io";
+import { FaRegUserCircle } from "react-icons/fa";
+import { GoFileDirectory } from "react-icons/go";
+import { RiUserFollowLine } from "react-icons/ri";
+import { IoAccessibilityOutline, IoAddCircleOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
   return (
-    <div className="fixed flex justify-around  b-0 bottom-0 text-white text-2xl w-full border-t-2  bg-gray-800 h-12 sm:hidden">
+    <div className="fixed flex justify-around   b-0 bottom-0 text-white text-2xl w-full border-t-2  bg-gray-800 h-14 sm:hidden">
       <div
         onClick={() => {
           handleNavigate("/");
         }}
-        className="flex flex-col items-center justify-center  text-center text-white  cursor-pointer hover:bg-purple-800 "
+        className="flex flex-col items-center justify-center  text-center text-white  cursor-pointer hover:text-purple-800 "
       >
         <AiOutlineHome className="" />
         <span className="text-sm">Home</span>
       </div>
-  
+
       <div
         onClick={() => {
-          handleNavigate("/");
+          handleNavigate("/collections");
         }}
-        className="flex flex-col items-center justify-center  text-2xl text-center text-white  cursor-pointer hover:bg-purple-800 "
+        className="flex flex-col items-center justify-center  text-center text-white  cursor-pointer hover:text-purple-800 "
       >
-      <IoIosAddCircleOutline className="text-xl w-10 h-10"/>
+        <GoFileDirectory />
+        <span className="text-sm">Collections</span>
       </div>
-  
-
 
       <div
         onClick={() => {
-          handleNavigate("/");
+          handleNavigate("/uploadvideo");
         }}
-        className="flex flex-col items-center justify-center  text-center text-white  cursor-pointer hover:bg-purple-800 "
+        className="flex flex-col items-center justify-center w-10 h-10 text-2xl text-center text-white  cursor-pointer hover:text-purple-800 "
       >
-        <AiOutlineHome className="" />
-        <span className="text-sm">You</span>
+        <IoIosAddCircleOutline className="w-full h-full" />
+      </div>
+
+      <div
+        onClick={() => {
+          handleNavigate("/profile");
+        }}
+        className="flex flex-col items-center justify-center  text-center text-white  cursor-pointer hover:text-purple-800 "
+      >
+        <RiUserFollowLine />
+        <span className="text-sm">Subscribers</span>
+      </div>
+
+      <div
+        onClick={() => {
+          handleNavigate("/profile");
+        }}
+        className="flex flex-col items-center justify-center  text-center text-white  cursor-pointer hover:text-purple-800 "
+      >
+        <FaRegUserCircle />
+        <span className="text-sm">Profile</span>
       </div>
     </div>
   );
