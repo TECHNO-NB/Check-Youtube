c7406e6b2485ca70c873054f4e1fe018e362b7c1
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-12-17 16:52:45 +0545

    fixed ui

--- src/pages/Subscribers.jsx
+++ src/pages/Subscribers.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Subscribers = () => {
   const [data, setData] = useState([]);
+ const navigate= useNavigate()
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.get(
@@ -24,8 +26,8 @@ const Subscribers = () => {
       <div className="pl-[0.0em] text-white mt-4 md:pl-[17em]">
         {data.map((val) => {
           return (
-            <div className="flex gap-4 mt-2 bg-blue-600 border-2 lg:w-[30%]" key={val._id}>
-                <img className="border-4" src={val.subscriberDetails.avatar} width={50} height={40}/>
+            <div className="flex gap-2 rounded-md mt-2 bg-gary-800 border-2 lg:w-[30%]" key={val._id}>
+                <img onClick={()=>navigate(`/otherprofile/${val.subscriberDetails.username}`)} className="border-4 cursor-pointer" src={val.subscriberDetails.avatar} width={50} height={50}/>
                 <div className="div">
                    <h1>{val.subscriberDetails.username}</h1>
                    <h1>{val.subscriberDetails.email}</h1>
