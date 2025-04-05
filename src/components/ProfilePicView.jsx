3722f80f1e9228f8148b4dbac1058211904201a6
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-04-05 15:14:10 +0545

    fixed user profile view

--- src/components/ProfilePicView.jsx
+++ src/components/ProfilePicView.jsx
@@ -5,11 +5,11 @@ import { IoCloseSharp } from "react-icons/io5";
 const ProfilePicView = ({props}) => {
     console.log(props)
   return (
-    <div className=" fixed inset-0 w-full flex justify-center items-center">
-      <div onClick={()=>props.setIsProfileView(false)} className="div relative top-[-15%] left-[17%] bg-black rounded-full p-2  cursor-pointer  ">
+    <div className=" fixed inset-0 w-full flex justify-center items-center z-50 ">
+      <div onClick={()=>props.setIsProfileView(false)} className="div fixed top-[28.5%] md:top-[30%] left-[58%] md:left-[53.5%] bg-black rounded-full p-2  cursor-pointer  ">
         <IoCloseSharp className="text-white text-2xl" />
       </div>
-      <div className="img rounded-[100%] w-72 h-72 ">
+      <div className="img rounded-[100%] w-72 h-72  bg-black">
         <img
           className="rounded-[100%] w-full h-full"
           src={props.avatar || logo}
