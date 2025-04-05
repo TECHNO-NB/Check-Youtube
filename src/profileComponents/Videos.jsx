8369e20905a7c856fb18bdcf213970d35c3f47d9
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-04-05 17:06:41 +0545

    add loader in profile page videos

--- src/profileComponents/Videos.jsx
+++ src/profileComponents/Videos.jsx
@@ -2,19 +2,24 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { FaRegCirclePlay } from "react-icons/fa6";
 import VideoCard from "../components/VideoCard";
+import Comloader from "../components/loader/Comloader";
 
 const Videos = () => {
   const [allVideos, setAllVideos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getuserVideos = async () => {
+      setIsLoading(true);
       try {
-        axios.defaults.withCredentials=true;
+        axios.defaults.withCredentials = true;
         const res = await axios.get(
           `${import.meta.env.VITE_BACKEND_URL}/api/v1/getuservideos`
         );
-      setAllVideos(res.data)
+        setIsLoading(false);
+        setAllVideos(res.data);
       } catch (error) {
+        setIsLoading(false);
         console.log(error);
       }
     };
@@ -22,16 +27,14 @@ const Videos = () => {
   }, []);
   return (
     <>
-
+    {isLoading ? <Comloader/> : null}
       {allVideos && allVideos.length > 0 ? (
-       <div className="videoList ">
-      {  allVideos.map((videoData) => (
-          <VideoCard key={videoData._id} data={videoData} />
-        ))
-      }
-      </div>
-      ) 
-      : (
+        <div className="videoList ">
+          {allVideos.map((videoData) => (
+            <VideoCard key={videoData._id} data={videoData} />
+          ))}
+        </div>
+      ) : (
         <div className=" flex  flex-col h-[15em]  items-center justify-center text-center text-white">
           <FaRegCirclePlay className="text-4xl" />
           <h1>No videos uploaded</h1>
